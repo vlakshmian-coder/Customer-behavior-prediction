f3e49f8825fed192758f27de41630cbf49fdef8f
vlakshmian-coder <vlakshmi.an@gmail.com>
2026-07-26 12:19:09 +0530

    Complete initial EDA setup and fix data loading module

---
+++ src/load_data.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+import pandas as pd
+
+def load_data():
+    project_root = Path(__file__).resolve().parent.parent
+    file_path = project_root / "data" / "processed" / "online_retail_cleaned.csv"
+
+    df = pd.read_csv(file_path)
+    return df
